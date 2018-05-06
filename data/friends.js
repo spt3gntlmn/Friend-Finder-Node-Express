9737675ab8ea502a622fee99fdf8e2b160ca613b
Samuel P Taylor III <s.p.taylor.3@gmail.com>
2018-05-06 06:07:42 -0400

    completed routes

--- data/friends.js
+++ data/friends.js
@@ -1,6 +1,6 @@
 let friends = [{
   "name": "Ahmed",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo": "https://i.pinimg.com/236x/7d/ef/41/7def41ff4e70b2ecba60187c54c260a8--caricatures.jpg",
   "scores": [
     5,
     1,
@@ -16,7 +16,7 @@ let friends = [{
 },
 {
   "name": "Max",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo": "https://s3.amazonaws.com/lowres.cartoonstock.com/films-mel_gibson-hollywood-lethal_weapon-what_women_want-conspiracy_theory-ate0003_low.jpg",
   "scores": [
     3,
     3,
@@ -29,12 +29,32 @@ let friends = [{
     5,
     2
   ]
+},
+{
+  'name': 'Billy Dude',
+  'photo': 'https://t5.rbxcdn.com/bf44a6240f1fdf54b0a85d49316b153c',
+  'scores': ['5', '5', '5', '5', '5', '5', '5', '5', '5', '4']
+},
+{
+  'name': 'Scarry Sal',
+  'photo': 'http://www.drawing-pencil-sketches.com/images/Rugby-face.jpg',
+  'scores': ['2', '5', '3', '2', '1', '4', '5', '4', '2', '1']
+},
+{
+  'name': 'Leaky Larry',
+  'photo': 'https://i.pinimg.com/originals/30/0d/f3/300df37cc04ecfea6194d4f23e04d58b.jpg',
+  'scores': ['1', '2', '2', '1', '2', '3', '4', '1', '2', '1']
+},
+{
+  'name': 'Burned Barb',
+  'photo': 'https://cartoonface.files.wordpress.com/2010/06/old-model-cartoon-face.jpg',
+  'scores': ['2', '4', '2', '1', '4', '2', '3', '2', '1', '4']
+},
+{
+  'name': 'Harry',
+  'photo': 'https://pbs.twimg.com/profile_images/712691559414652928/Hxya7djO.jpg',
+  'scores': ['3', '2', '4', '4', '5', '5', '4', '3', '2', '1']
 }
 ]
-
-
-{
-  Name: 'Samuel'
-}
 
 module.exports = friends;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,41 +19,27 @@ module.exports = function (app) {
       let deltaScore = 0;
 
       wannaBeFriendScore.forEach((friendScore, index2) => {
-        console.log(`Friends Array index: ${index2}.
-        Friend score: ${friendScore}.`)
-
-        console.log(`Friends Array scores: ${friendsArray[index1].scores[index2]}.`)
-        // deltaScore += (Math.abs(parseInt(friendsArray.scores[index]) - parsenInt(friendScore)));
+        deltaScore += (Math.abs(parseInt(friendsArray[index1].scores[index2]) - parseInt(friendScore)));
       });
-      // console.log(deltaScore);
+      console.log(deltaScore);
 
-      friendsArray.push(deltaScore);
+      friendScores.push(deltaScore);
     });
 
+    friendScores.forEach((score, index) => {
+      if (score <= friendScores[bestMatch]) {
+        bestMatch = index;
+        console.log(friendsArray[bestMatch]);
+      }
+    })
 
+    let yourNewFriend = friendsArray[bestMatch];
 
+    console.log(yourNewFriend);
+    res.json(yourNewFriend);
 
+    friendsArray.push(req.body);
 
-
-
-
-
-
-
-
-    // console.log("I'm pushing to the friends array")
-    // friends.push(req.body);
-    // console.log(parseInt(req.body.scores[0]));
-    // let sum = 0;
-    // for (var i = 0; i < req.body.scores.length; i++) {
-    //   friends.push(req.body.scores[i]);
-    //   sum = sum + parseInt(req.body.scores[i]);
-    //   var avg = sum / (i + 1);
-    // }
-    // console.log(`This is the average score: ${avg}.
-    // This is the friends array: ${friends}.`);
-    // console.log(friends);
-    res.send("200");
   });
 
   // ---------------------------------------------------------------------------
